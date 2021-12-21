b89e765cc4c5714c457efa3cf53f6b92c11ade13
Takashi Matsuo <tmatsuo@google.com>
2021-12-20 16:43:25 -0800

    chore: migrate 10 APIs to owlbot (#4845)
    - ServiceControl
    - ServiceDirectory
    - ServiceManagement
    - ServiceUsage
    - Shell
    - Spanner
    - Speech
    - SqlAdmin
    - StorageTransfer
    - Talent

--- owlbot.py
+++ owlbot.py
@@ -14,33 +14,38 @@
 
 """This script is used to synthesize generated parts of this library."""
 
-import subprocess
-import synthtool as s
-import synthtool.gcp as gcp
 import logging
+from pathlib import Path
+import subprocess
 
-AUTOSYNTH_MULTIPLE_COMMITS = True
+import synthtool as s
+from synthtool.languages import php
+from synthtool import _tracked_paths
 
 logging.basicConfig(level=logging.DEBUG)
 
-gapic = gcp.GAPICBazel()
+src = Path(f"../{php.STAGING_DIR}/ServiceDirectory").resolve()
+dest = Path().resolve()
 
-for version in ['v1', 'v1beta1']:
-    library = gapic.php_library(
-        service="servicedirectory",
-        version=version,
-        bazel_target=f'//google/cloud/servicedirectory/{version}:google-cloud-servicedirectory-{version}-php',
-    )
+# Added so that we can pass copy_excludes in the owlbot_main() call
+_tracked_paths.add(src)
 
-    # copy all src
-    s.move(library / f"src")
+php.owlbot_main(
+    src=src,
+    dest=dest,
+    copy_excludes=[
+        src / "**/*_*.php"
+    ]
+)
 
-    # copy proto files to src also
-    s.move(library / f"proto/src/Google/Cloud/ServiceDirectory", f"src/")
-    s.move(library / f"tests/")
-
-    # copy GPBMetadata file to metadata
-    s.move(library / f'proto/src/GPBMetadata/Google/Cloud/Servicedirectory', f"metadata/")
+# remove class_alias code
+s.replace(
+    "src/V*/*/*.php",
+    r"^// Adding a class alias for backwards compatibility with the previous class name.$"
+    + "\n"
+    + r"^class_alias\(.*\);$"
+    + "\n",
+    '')
 
 # document and utilize apiEndpoint instead of serviceAddress
 s.replace(
@@ -59,30 +64,6 @@ s.replace(
     "**/Gapic/*GapicClient.php",
     r"\$transportConfig, and any \$serviceAddress",
     r"$transportConfig, and any `$apiEndpoint`")
-
-# fix year
-s.replace(
-    "src/V1/**/*.php",
-    r"Copyright \d{4}",
-    r"Copyright 2021")
-s.replace(
-    "tests/Unit/V1/*Test.php",
-    r"Copyright \d{4}",
-    r"Copyright 2021")
-s.replace(
-    "src/V1beta1/**/*.php",
-    r"Copyright \d{4}",
-    r"Copyright 2020")
-s.replace(
-    "tests/Unit/V1beta1/*Test.php",
-    r"Copyright \d{4}",
-    r"Copyright 2020")
-
-# Change the wording for the deprecation warning.
-s.replace(
-    "src/*/*_*.php",
-    r"will be removed in the next major release",
-    "will be removed in a future release")
 
 ### [START] protoc backwards compatibility fixes
 
